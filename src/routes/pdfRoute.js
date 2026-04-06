1c54069ca8dbcfff6de0ff28bcf59a35c747033a
Jvsilvagomes <joao.silvassenai@gmail.com>
2026-04-06 11:12:08 -0300

    feat: ajusta pdf e arruma erros de variaveis

--- src/routes/pdfRoute.js
+++ src/routes/pdfRoute.js
@@ -4,8 +4,7 @@ import autenticar1 from './../utils/apiKey.js';
 
 const router = express.Router();
 
-router.get('/pdf', autenticar1 ,controller.buscarTodos);
+router.get('/pdf', autenticar1, controller.informacoesTodos);
 router.get('/:id/pdf', autenticar1, controller.informacoesPorId);
-
 
 export default router;
